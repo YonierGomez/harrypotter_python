00e202b96a3f1f753d9afbe6bbdac93e375371fc
Yonier Asprilla <Yonieer13@gmail.com>
2024-04-15 20:12:20 -0500

    Add module

--- harry_potter_yonier/get_one_person.py
+++ harry_potter_yonier/get_one_person.py
@@ -8,8 +8,10 @@ def obtener_personaje(URL, PERSONAJE):
         
         for r in respuesta:
             if PERSONAJE.lower() in r["name"].lower():
-                print(f"Hola, soy {r['name']} y pertenezco a la casa {r['house']}.")
+                # print(f"Hola, soy {r['name']} y pertenezco a la casa {r['house']}.")
+                return f"Hola, soy {r['name']} y pertenezco a la casa {r['house']}."
 
 if __name__ == "__main__":
     buscar_personaje = input("Ingrese el nombre del personaje que desea buscar: ")
-    obtener_personaje(URL="https://hp-api.onrender.com/api/characters", PERSONAJE=buscar_personaje)
+    final = obtener_personaje(URL="https://hp-api.onrender.com/api/characters", PERSONAJE=buscar_personaje)
+    print(final)
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from harry_potter_yonier import get_one_person
 if __name__ == "__main__":
-    print("Lab desde python")
+    buscar_personaje = input("Ingrese el nombre del personaje que desea buscar: ")
+    final = get_one_person.obtener_personaje(URL="https://hp-api.onrender.com/api/characters", PERSONAJE=buscar_personaje)
+    print(final)
